b68e3b393159b77838fda4cd8fe8bbee12b8dd45
Yaroslav Romashin <kingofpirates119977@gmail.com>
2022-08-17 18:59:48 +0700

    used ReaderT with Config

--- app/Main.hs
+++ app/Main.hs
@@ -8,12 +8,15 @@ import qualified Data.ByteString.Char8 as BS (pack, putStrLn)
 import Data.Aeson (decodeStrict)
 import Parsing
 import Utils
+import Control.Monad.Trans.Reader (ReaderT, runReaderT, ask)
+import Control.Monad.Trans.Class (lift)
+
 
 
 data Config = Config {token :: String, help :: String, repeat' :: Int}
 
 main :: IO ()
-main = do 
+main = do
   config <- parseConfig
   case findKey "token" config of
     Nothing -> putStrLn "Couldn't parse token from config"
@@ -21,7 +24,7 @@ main = do
       Nothing -> putStrLn "Couldn't parse /help from config"
       Just help -> case findKey "/repeat" config of
         Nothing -> putStrLn "Couldn't parse /repeat from config"
-        Just repeat' -> botLoop 0 (Config token help (read repeat' :: Int)) [] []
+        Just repeat' -> runReaderT (botLoop 0 [] []) (Config token help (read repeat' :: Int))
   
 sendMessage :: String -> UserMessage -> Int -> IO ()
 sendMessage _ _ 0 = pure ()
@@ -57,48 +60,50 @@ removeKeyboard token msg update chatId = do
   httpBS json
   pure ()
 
-botLoop :: Int -> Config -> [Int] -> [(Int, Int)] -> IO () 
-botLoop offset cfg acc acc2 = do
-  response <- httpBS (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token cfg ++ "/getUpdates?offset=" ++ (show offset))
+botLoop :: Int -> [Int] -> [(Int, Int)] -> ReaderT Config IO () 
+botLoop offset acc acc2 = do
+  cfg <- ask
+  response <- lift $ httpBS (parseRequestThrow_ $ "https://api.telegram.org/bot" ++ token cfg ++ "/getUpdates?offset=" ++ (show offset))
   let mbTgRes = decodeStrict $ getResponseBody response :: Maybe TelegramResult
   case mbTgRes of
-    Nothing -> putStrLn $ "Invalid Json"
+    Nothing -> lift $ putStrLn $ "Invalid Json"
     Just tgRes -> do
       let userMessages = result tgRes
       let updateIds = map getUpdId userMessages
       let newOffset = if null updateIds then 0 else maximum updateIds + 1
-      (allRepeat, userSettings) <- handleUpdates cfg userMessages acc acc2
-      botLoop newOffset cfg allRepeat userSettings
+      (allRepeat, userSettings) <- handleUpdates userMessages acc acc2
+      botLoop newOffset allRepeat userSettings
 
-handleUpdates :: Config -> [UserMessage] -> [Int] -> [(Int, Int)] -> IO ([Int], [(Int, Int)])
-handleUpdates _ [] acc acc2 = pure (acc, acc2)
-handleUpdates (Config token help repeat') (x:xs) acc acc2 =
+handleUpdates :: [UserMessage] -> [Int] -> [(Int, Int)] -> ReaderT Config IO ([Int], [(Int, Int)])
+handleUpdates [] acc acc2 = pure (acc, acc2)
+handleUpdates (x:xs) acc acc2 = do
+  cfg <- ask
   case x of
     TextMessage text updId chatId -> 
       case text of
         "/help" -> do
-          sendMessage token (TextMessage help updId chatId) 1
-          handleUpdates (Config token help repeat') xs acc acc2
+          lift $ sendMessage (token cfg) (TextMessage (help cfg) updId chatId) 1
+          handleUpdates xs acc acc2
         "/repeat" -> do
-          sendKeyboard token ("Tell me, how much times I should repeat your messages. From 1 to 5:") updId chatId
-          handleUpdates (Config token help repeat') xs (chatId:acc) acc2
+          lift $ sendKeyboard (token cfg) ("Tell me, how much times I should repeat your messages. From 1 to 5:") updId chatId
+          handleUpdates xs (chatId:acc) acc2
         _ -> 
           if chatId `elem` acc
             then do
               let userSettings = (chatId, read text :: Int):acc2
-              removeKeyboard token ("Ok, Now I will repeat your messages " ++ text ++ " times") updId chatId
+              lift $ removeKeyboard (token cfg) ("Ok, Now I will repeat your messages " ++ text ++ " times") updId chatId
               let newAcc = deleteFromList chatId acc []
-              handleUpdates (Config token help repeat') xs newAcc userSettings
+              handleUpdates xs newAcc userSettings
             else do
               case lookup chatId acc2 of
-                Just x -> sendMessage token (TextMessage text updId chatId) x
-                Nothing -> sendMessage token (TextMessage text updId chatId) repeat'
-              handleUpdates (Config token help repeat') xs acc acc2        
+                Just x -> lift $ sendMessage (token cfg) (TextMessage text updId chatId) x
+                Nothing -> lift $ sendMessage (token cfg) (TextMessage text updId chatId) (repeat' cfg)
+              handleUpdates xs acc acc2        
     StickerMessage fileId _ chatId -> do
       case lookup chatId acc2 of
-        Just x -> sendSticker token fileId chatId x
-        Nothing -> sendSticker token fileId chatId repeat'
-      handleUpdates (Config token help repeat') xs acc acc2
+        Just x -> lift $ sendSticker (token cfg) fileId chatId x
+        Nothing -> lift $ sendSticker (token cfg) fileId chatId (repeat' cfg)
+      handleUpdates xs acc acc2
 
 getUpdId :: UserMessage -> Int
 getUpdId (TextMessage _ updateId _ ) = updateId
